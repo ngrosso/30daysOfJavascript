01f7cc314b29189cc3ed517adfc1a504f80a26d7
Nicolas Grosso <nicolas.grosso@watea.com.ar>
2022-12-19 12:28:57 -0300

    initial commit, adding the first introduction chapter

---
+++ 01_Introduction/js/datatypes.js
@@ -0,0 +1,4 @@
+console.log("value of s:",s,"typeof s:", typeof(s))
+console.log("value of b:",b,"typeof b:", typeof(b))
+console.log("value of u:",u,"typeof u:", typeof(u))
+console.log("value of n:",n,"typeof n:", typeof(n))
---
+++ 01_Introduction/js/variable.js
@@ -0,0 +1,26 @@
+let s = "Dog"
+let b = true
+let u = undefined
+let n = null
+
+//unassigned variables
+let uv1, uv2, uv3, uv4
+
+//assigned variables
+let av1 = "Hello"
+let av2 = 2133
+let av3 = false
+let av4 = [1,2,3,4]
+
+//assigned in single line
+let nameS = "Nicolás" , lastNameS = "Grosso", maritalStatusS = "Relationship", countryS = "Argentina", ageS = 29
+
+//assigned in multi lines
+let nameM = "Nicolás"
+let lastNameM = "Grosso"
+let maritalStatusM = "Relationship" 
+let countryM = "Argentina"
+let ageM = 29
+
+console.log(`I am ${ageM} years old.`)
+console.log(`You are ${ageM+5} years old.`)
